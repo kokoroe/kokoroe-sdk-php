f335a14b0a0fe931ecc2173ef0a6f3f4abf443d4
Axel Etcheverry <euskadi31@gmail.com>
2016-04-15 18:29:32 +0200

    chore(sdk): change version

--- src/Kokoroe.php
+++ src/Kokoroe.php
@@ -31,7 +31,7 @@ class Kokoroe implements LoggerAwareInterface
     /**
      * @const string Kokoroe SDK version
      */
-    const VERSION = '1.0.0-alpha.2';
+    const VERSION = '1.0.0-alpha.3';
 
     /**
      * @const string Production API URL.
